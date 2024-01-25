65921eadca68c46d2efeb604cc98829dcd0a8323
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-01-25 11:16:41 +0100

    Solved a test case

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 Alpha1 = chr(122)
 for i in range(13):
     Alpha2 = chr(ord(Alpha1) - 33)
-    print(Alpha1, Alpha2, sep="", end="")
+    print("{}{}".format(Alpha1, Alpha2), end="")
     Alpha1 = chr(ord(Alpha1) - 2)
