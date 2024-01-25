69b2743ca0eecb016662ee86f6c01647dca97528
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-01-25 07:22:04 +0100

    Done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
+        alpha = str[i]
         if str[i] >= "a" and str[i] <= "z":
             num = ord(str[i]) - 32
             alpha = chr(num)
             if i == len(str) - 1:
                 alpha = alpha + "\n"
-            print("{}".format(alpha), end="")
+        print("{}".format(alpha), end="")
+
+uppercase("Holberton school")
