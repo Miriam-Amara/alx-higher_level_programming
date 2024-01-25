b7988f191fdbbc290946a003203902f1aa9efa3c
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-01-25 07:03:38 +0100

    Done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def uppercase(str):
     for i in range(len(str)):
         if str[i] >= "a" and str[i] <= "z":
@@ -9,7 +7,3 @@ def uppercase(str):
             if i == len(str) - 1:
                 alpha = alpha + "\n"
             print(f"{alpha}", end="")
-        else:
-            if i == len(str) - 1:
-                alpha = alpha + "\n"
-            print(f"{str[i]}", end="")
