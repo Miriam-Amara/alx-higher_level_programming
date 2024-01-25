c064a5a4deed261856ab66783d6af7aede3b2cda
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-01-25 11:56:24 +0100

    reduced the code

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-Alpha1 = chr(122)
-for i in range(13):
-    Alpha2 = chr(ord(Alpha1) - 33)
-    print("{}{}".format(Alpha1, Alpha2), end="")
-    Alpha1 = chr(ord(Alpha1) - 2)
+for i in range(122, 97, -2):
+    print("{:c}{:c}".format(i, (i -33)), end = "")
