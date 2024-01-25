ca7ca61389ead8e6f18dd15b6b8ae0cf37868091
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-01-25 09:09:58 +0100

    Done

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
-
 def print_last_digit(number):
     number = str(number)
-    for i in range(len(number)):
-        if i == len(number) - 1:
-            print(f"{number[i]}", end="")
-            return number[i]
+    print(f"{int(number[-1])}", end="")
+    return int(number[-1])
