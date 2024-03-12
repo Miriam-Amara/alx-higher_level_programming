eaeba9e67d8d26ae12dc57e6ec56f1cd58a386af
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-03-12 03:57:53 +0100

    refactored code

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,8 @@
 #!/usr/bin/pyth0n3
 def only_diff_elements(set_1, set_2):
-    list1 = list(set_1)
-    list2 = list(set_2)
-    list3 = list1 + list2
-    for item in list1:
-        if item in list2:
-            list3.remove(item)
-            list3.remove(item)
-    return set(list3)
+    list_elements = list(set_1) + list(set_2)
+    for item in set_1:
+        if item in set_2:
+            list_elements.remove(item)
+            list_elements.remove(item)
+    return set(list_elements)
