f685df34595cfb12a825078e2555c9e31ecaf4c5
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-03-12 03:29:16 +0100

    refactored

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_3 = set({})
-    new_list = list(set_1) + list(set_2)
-    for i, item in enumerate(new_list):
-        del new_list[i]
-        if item in new_list:
-            set_3.add(item)
-    return(set_3)
+    elements = set(list(set_1) + list(set_2))
+    common_elements = []
+    for value in elements:
+        if value in set_1 and value in set_2:
+            common_elements.append(value)
+    return common_elements
