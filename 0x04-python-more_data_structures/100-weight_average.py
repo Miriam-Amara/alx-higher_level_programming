cd297db63d5d421ea677bad76fbbc8b19bb58667
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-03-12 05:20:19 +0100

    refactored code

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    sum_num = 0
-    weights = []
+    sum_num, sum_weights = (0, 0)
     if not my_list:
         return 0
     for tuples in my_list:
         for i, j in [tuples]:
-            weights.append(j)
             mul = i * j
         sum_num += mul
-    weight_average = sum_num / sum(weights)
-    return weight_average
+        sum_weights += j
+    return (sum_num / sum_weights)
