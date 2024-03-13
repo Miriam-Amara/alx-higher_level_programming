3ae332831e7124518117dd0311750327685f822e
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-03-13 05:11:12 +0100

    fixed a requirement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end = '')
     except IndexError:
         print()
-        return len(my_list)
+        return (i)
     print()
-    return (x)
+    return (i + 1)
