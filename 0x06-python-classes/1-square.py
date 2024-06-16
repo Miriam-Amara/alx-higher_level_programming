bb35417531e5b321b7c04218fad70424a0c6950f
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-06-16 08:24:02 +0100

    defined square class with 1 arg

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+A module that defines an empty Square class
+"""
+
+
+class Square:
+    """
+    A class that calculates squares of numbers
+
+    Attributes:
+    __size: the number to be squared
+    """
+
+    def __init__(self, size):
+        self.__size = size
