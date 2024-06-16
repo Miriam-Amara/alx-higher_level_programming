1048de0c04616385acad2391c110d591f5416561
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-06-16 10:24:47 +0100

    updated Square class with area method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+A module that defines a Square class
+"""
+
+
+class Square:
+    """
+    This class that calculates square of a number
+
+    Attributes:
+    __size(int): the number to be squared
+
+    Methods:
+    area(self): returns the square of the number
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the instance attriutes
+
+        Args:
+        size(int): the number to be squared, default is 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the square of a number
+        """
+        return self.__size ** 2
