a0be4dd5c4301b604a132d3d527f99698f91e73b
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-06-18 07:19:22 +0100

    created add_integer function and it's doctest

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module provides functions for basic arithmetic operations.
+
+Functions:
+add_integer(a, b=98) -> int: Returns the sum of a and b.
+
+Usage:
+Import the module and call the function with at least one argument:
+
+import 0-add_integer
+
+result = 0-add_integer.add_integer(3, 4)
+print(result) #Output: 7
+"""
+
+
+def add_integer(a, b=98) -> int:
+    """
+    This function computes the sum of two numbers.
+    
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number. It is by default 98.
+    
+    Returns:
+    int: The sum of a and b.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
