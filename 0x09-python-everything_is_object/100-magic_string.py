6bcc155ce5386e571d65ea20fed201293c75bdaa
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-10-16 02:11:53 +0100

    meet alx requirement

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.count_call += 1
-    return "BestSchool, " * (magic_string.count_call - 1) + "BestSchool"
-
-
+    magic_string.count_call += 1; return "BestSchool, " * (magic_string.count_call - 1) + "BestSchool"
 magic_string.count_call = 0
