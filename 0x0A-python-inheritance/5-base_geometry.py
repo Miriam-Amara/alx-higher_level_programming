0b05b9c55c099b9d6a741985588a22c0e0df75a3
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-10-18 20:25:45 +0100

    add an empty BaseGeometry class.

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+BaseGeometry Module
+
+This module provides an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    pass
+
+
+# bg = BaseGeometry()
+
+# print(bg)
+# print(dir(bg))
+# print(dir(BaseGeometry))
