bb2464ae7861afe89fc989fb55b4245ac5600de1
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-10-22 17:06:58 +0100

    add module and function docstrings

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 
+"""
+This module provides read_file function that
+reads the contents of a file.
+"""
+
+
 def read_file(filename=""):
+    """
+    Opens and reads the contents of a file.
+
+    Args:
+        filename (str): Name of the file to be read.
+    """
     with open(filename) as f:
         content = f.read()
         print(content)
+
 
 # read_file("my_file.txt")
