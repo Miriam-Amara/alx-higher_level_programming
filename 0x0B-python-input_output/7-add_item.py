817175dda7f624ab7cf289a696b991b9cf266664
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-10-23 16:25:29 +0100

    adds add_item function

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+This module provides `add_item` function that adds all arguments
+to a Python list, and then save them to a file
+"""
+
+
+import sys
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+def add_item():
+    """
+    Adds all arguments to a Python list, and then save them to a file.
+    """
+    arguments = sys.argv
+    filename = "add_item.json"
+
+    try:
+        List = load_from_json_file(filename)
+    except FileNotFoundError:
+        List = []
+    for arg in arguments[1:]:
+        List.append(arg)
+    save_to_json_file(List, filename)
