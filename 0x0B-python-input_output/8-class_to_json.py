556c64c942fd16900cc5db8924d99f0802e50c2c
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-10-23 16:26:15 +0100

    add class that returns object dict

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+This module provides `class_to_json` function,
+which returns the dictionary description of an object
+with simple data structures suitable for JSON serialization.
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary representation of an object
+    with simple data structures (list, dict, str, int, bool)
+    for JSON serializaton.
+
+    Args:
+        obj (Any): A class instance to be converted into a dictionary.
+
+    Returns:
+        dict: A dictionary containing all attributes of the object,
+        suitable for JSON serialization.
+    """
+    return obj.__dict__
