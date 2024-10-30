816914332b46d02c7178b1aaf772d26bf830562d
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-10-30 10:31:28 +0100

    add Base class and its unittest

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,12 @@
+Project review on:
+
+Import
+Exceptions
+Class
+Private attribute
+Getter/Setter
+Class method
+Static method
+Inheritance
+Unittest
+Read/Write file
---
+++ 0x0C-python-almost_a_circle/tests/base_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""
+base_test module
+
+This module provides BaseTest class, which serves as
+unittest for base module
+"""
+
+
+import unittest
+import sys
+import os
+from base import Base
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../models"))
+)
+
+
+class BaseTest(unittest.TestCase):
+    """
+    A class used for testing the base class.
+    """
+
+    def setUp(self):
+        pass
+
+    def test_init_method(self):
+        """
+        Test Base initialization with specific and default ids.
+        """
+        input_ids = [3, 0, None, 12]
+        expected_ids = [1, 3, 2, 0, 3, 4, 5, 12]
+        input_index = 0
+
+        for i in range(8):
+            if i % 2:
+                # use id from input_ids
+                b = Base(id=input_ids[input_index])
+                self.assertEqual(b.id, expected_ids[i])
+                input_index += 1
+            else:
+                # default id (auto-increment)
+                b = Base()
+                self.assertEqual(b.id, expected_ids[i])
+
+
+if __name__ == "__main__":
+    unittest.main()
