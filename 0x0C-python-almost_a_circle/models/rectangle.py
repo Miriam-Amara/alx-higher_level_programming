7181ea7fa56720c696b451eafdb2bebe657e58f7
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-10-30 18:28:50 +0100

    add Rectangle class and its unittest

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+"""
+Module for Rectangle class that inherits from Base.
+"""
+
+
+from base import Base
+
+
+class Rectangle(Base):
+    """Represents a rectangle defined by its width, height, and position."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new rectangle with given dimensions and position."""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width; must be positive."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be greater than 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height; must be positive."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be greater than 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get the x-coordinate of the rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set the x coordinate; must be an integer or float."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("x must be an integer")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get the y-coordinate of the rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set the y coordinate; must be an integer or float."""
+        if not isinstance(value, (int, float)):
+            raise TypeError("y must be an integer")
+        self.__y = value
+
+
+if __name__ == "__main__":
+    rectangle = Rectangle(5, 8)
+    print(rectangle.width)
---
+++ 0x0C-python-almost_a_circle/tests/rectangle_test.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+"""
+Unit tests for the Rectangle class.
+
+This module tests the Rectangle class for correct initialization
+and error handling.
+
+Classes:
+    TestRectangle: Contains tests for the Rectangle class.
+
+Methods:
+    setUp: Initializes test variables.
+    test_valid_initialization: Validates correct Rectangle creation.
+    test_value_error: Checks for ValueErrors with invalid parameters.
+    test_type_error: Checks for TypeErrors with incorrect types.
+"""
+
+import unittest
+import sys
+import os
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../models"))
+)
+
+from rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """
+    Tests for the Rectangle class.
+    """
+
+    def setUp(self):
+        """
+        Initialize test variables.
+        """
+        self.a = 5
+        self.b = 80
+        self.c = -7
+        self.d = 0
+        self.e = 7.6
+        self.list1 = [4, 5]
+        self.tuple1 = (4, -4)
+        self.set1 = {5, "j"}
+        self.dict1 = {"name": "x", 1: 5}
+        self.str1 = " "
+
+    def test_valid_initialization(self):
+        """
+        Test valid Rectangle initialization.
+        """
+        obj = Rectangle(self.a, self.b)
+        self.assertEqual(obj.width, self.a)
+        print(obj.width)
+        self.assertEqual(obj.height, self.b)
+        self.assertEqual(obj.x, 0)
+        self.assertEqual(obj.y, 0)
+
+        obj1 = Rectangle(self.e, self.a, x=self.e, y=self.b, id=8)
+        self.assertEqual(obj1.width, self.e)
+        self.assertEqual(obj1.height, self.a)
+        self.assertEqual(obj1.x, self.e)
+        self.assertEqual(obj1.y, self.b)
+        self.assertEqual(obj1.id, 8)
+
+    def test_value_error(self):
+        """
+        Test ValueErrors for invalid parameters.
+        """
+        with self.assertRaises(ValueError):
+            obj2 = Rectangle(self.c, self.a)
+            print(obj2.width, "width")
+        with self.assertRaises(ValueError):
+            obj2 = Rectangle(self.a, self.d)
+
+    def test_type_error(self):
+        """
+        Test TypeErrors for invalid types.
+        """
+        with self.assertRaises(TypeError):
+            obj3 = Rectangle(self.list1, self.a)
+        with self.assertRaises(TypeError):
+            obj3 = Rectangle(self.a, self.b, self.a, self.set1)
+        with self.assertRaises(TypeError):
+            obj3 = Rectangle(self.tuple1, self.a)
+        with self.assertRaises(TypeError):
+            obj3 = Rectangle(self.a, self.dict1)
+        with self.assertRaises(TypeError):
+            obj3 = Rectangle(self.b, self.a, self.str1)
+
+
+if __name__ == "__main__":
+    unittest.main()
