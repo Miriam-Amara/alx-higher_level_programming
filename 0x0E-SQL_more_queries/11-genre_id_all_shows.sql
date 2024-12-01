383f75baef040c84668b57c7b7225978002c4f4d
Miriam-Amara <ikechukwumiriam@gmail.com>
2024-12-01 14:18:42 +0100

    validate alx requirement

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,13 +1,13 @@
 -- host: localhost  database: hbtn_0d_tvshows
-/*
-lists all shows contained in the database hbtn_0d_tvshows.
 
-- Each record should display: tv_shows.title - tv_show_genres.genre_id
-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-- If a show doesn’t have a genre, display NULL
-- Use only one SELECT statement
-- The database name will be passed as an argument of the mysql command
-*/
+-- lists all shows contained in the database hbtn_0d_tvshows.
+
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NULL
+-- Use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
 
 SELECT title, genre_id
 FROM tv_shows AS ts
